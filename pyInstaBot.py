af1ce8b97e678908d81f3119a110cbecd0a8d331
Nils Schaetti <n.schaetti@gmail.com>
2017-09-03 23:20:08 +0200

    Main file

---
+++ pyInstaBot.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+
+# Imports
+from instagram.Instagram import Instagram
+
+
+if __name__ == "__main__":
+
+    instagram = Instagram(username="", password="")
+
+# end if
