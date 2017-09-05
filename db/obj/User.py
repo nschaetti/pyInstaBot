6235a1d7d7f96d7967dc04a15f2cb1ed2e2d4954
Nils Schaetti <n.schaetti@gmail.com>
2017-09-06 00:24:14 +0200

    User object

---
+++ db/obj/User.py
@@ -0,0 +1,150 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# File : Action.py
+# Description : pyTweetBot Action in the DB.
+# Auteur : Nils Schaetti <n.schaetti@gmail.com>
+# Date : 01.05.2017 17:59:05
+# Lieu : Nyon, Suisse
+#
+# This file is part of the pyTweetBot.
+# The pyTweetBot is a set of free software:
+# you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# pyTweetBot is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with pyTweetBar.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+# Import
+from db.DBConnector import DBConnector
+from sqlalchemy import Column, BigInteger, Boolean, Unicode, Date
+from .Base import Base
+
+
+# Instagram User
+class User(Base):
+    """
+    Action
+    """
+
+    # Table name
+    __tablename__ = "pyinstb_users"
+
+    # Fields
+    user_id = Column(BigInteger, primary_key=True)
+    user_username = Column(Unicode(500), nullable=False)
+    user_full_name = Column(Unicode(2000), nullable=False)
+    user_profile_pic_url = Column(Unicode(5000), nullable=False)
+    user_is_verified = Column(Boolean, nullable=False)
+    user_followed_by_viewer = Column(Boolean, nullable=False)
+    user_requested_by_viewer = Column(Boolean, nullable=False)
+    user_is_follower = Column(Boolean, nullable=True)
+    user_is_following = Column(Boolean, nullable=True)
+    user_follower_date = Column(Date, nullable=True)
+    user_following_date = Column(Date, nullable=True)
+
+    ############################################
+    # Public
+    ############################################
+
+    ############################################
+    # Override
+    ############################################
+
+    # Exists
+    @staticmethod
+    def exists(user_id):
+        """
+        Exists
+        :param user_id:
+        :return:
+        """
+        return DBConnector().get_session().query(User).filter(user_id == user_id).count() > 0
+    # end exists
+
+    # Is follower
+    @staticmethod
+    def is_follower(user_id):
+        """
+        Is follower?
+        :param user_id:
+        :return:
+        """
+        if not User.exists(user_id):
+            return False
+        else:
+            user = User.get(user_id)
+            return user.user_is_follower
+        # end if
+    # end is_follower
+
+    # Is following
+    @staticmethod
+    def is_following(user_id):
+        """
+        Is following?
+        :param user_id:
+        :return:
+        """
+        if not User.exists(user_id):
+            return False
+        else:
+            user = User.get(user_id)
+            return user.user_is_following
+            # end if
+    # end is_following
+
+    # Get user
+    @staticmethod
+    def get(user_id):
+        """
+        Get user
+        :param user_id:
+        :return:
+        """
+        return DBConnector().get_session().query(User).filter(user_id == user_id).all()[0]
+    # end get
+
+    ############################################
+    # Override Functions
+    ############################################
+
+    # To unicode
+    def __unicode__(self):
+        """
+        To unicode
+        :return:
+        """
+        return u"(user_id={},  username={}, full_name={}, pic_url={}, " \
+               u"is_verified={}, followed_by_viewer={}, requested_by_viewer={}, "\
+               u"is_follower={}, is_following={}, follower_date={}, following_date={})".\
+            format(self.user_id, self.user_username, self.user_full_name, self.user_profile_pic_url,
+                   self.user_is_verified, self.user_followed_by_viewer, self.user_requested_by_viewer,
+                   self.user_is_follower, self.user_is_following, self.user_follower_date,
+                   self.user_following_date)
+    # end __unicode__
+
+    # To string
+    def __str__(self):
+        """
+        To unicode
+        :return:
+        """
+        return "(user_id={},  username={}, full_name={}, pic_url={}, " \
+               "is_verified={}, followed_by_viewer={}, requested_by_viewer={})" \
+               "is_follower={}, is_following={}, follower_date={}, following_date={})".\
+            format(self.user_id, self.user_username, self.user_full_name.encode('utf-8', errors='ignore'),
+                   self.user_profile_pic_url,
+                   self.user_is_verified, self.user_followed_by_viewer, self.user_requested_by_viewer,
+                   self.user_is_follower, self.user_is_following, self.user_follower_date,
+                   self.user_following_date)
+    # end __str__
+
+# end Action
