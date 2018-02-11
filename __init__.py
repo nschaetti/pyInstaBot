e5da110c322b4ed5ce3355ae1d73f393f0c167db
nschaetti <n.schaetti@gmail.com>
2018-02-11 16:29:03 +0100

    Main __init__.py

---
+++ __init__.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# File : __init__.py
+# Description : Main init file.
+# Auteur : Nils Schaetti <n.schaetti@gmail.com>
+# Date : 11.02.2018 13:51:05
+# Lieu : Nyon, Suisse
+#
+# This file is part of the pyInstaBot.
+# The pyInstaBot is a set of free software:
+# you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# pyTweetBot is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with pyTweetBar.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+# Imports
+import config
+import db
+import followers
+import gui
+import instagram
+import learning
+import patterns
+
+# All
+__all__ = ['config', 'db', 'followers', 'gui', 'instagram', 'learning', 'patterns']
