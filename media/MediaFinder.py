0b6acab132764c201d562dd7f12c088b679a18e6
nschaetti <n.schaetti@gmail.com>
2018-03-03 22:08:32 +0100

    Media finder

--- media/MediaFinder.py
+++ media/MediaFinder.py
@@ -8,6 +8,8 @@ from textblob import TextBlob
 import time
 import logging
 import pyInstaBot.tools.strings as pystr
+import pyInstaBot.instagram
+import random
 
 
 # Class to find media to like or comment
@@ -17,7 +19,7 @@ class MediaFinder(object):
     """
 
     # Constructor
-    def __init__(self, search_keywords="", polarity=0.0, subjectivity=0.5, languages=['en']):
+    def __init__(self, search_keywords="", shuffle=False, polarity=0.0, subjectivity=0.5, languages=['en']):
         """
         Constructor
         """
@@ -27,17 +29,13 @@ class MediaFinder(object):
         self._subjectivity = subjectivity
         self._languages = languages
 
-        # Cursor
-        if search_keywords == "":
-            # self._cursor = pyInstaBot.instagram.InstagramConnector().get_time_line(n_pages)
-            pass
-        else:
-            # self._cursor = pyInstaBot.instagram.InstagramConnector().search_tweets(search_keywords, n_pages)
-            pass
-        # end if
+        # Feed
+        self._medias = pyInstaBot.instagram.InstagramConnector().hashtag_feed(search_keywords)['ranked_items']
 
-        # Current list of medias
-        self._medias = list()
+        # Shuffle
+        if shuffle:
+            random.shuffle(self._medias)
+        # end if
     # end __init__
 
     #############################################
@@ -59,9 +57,8 @@ class MediaFinder(object):
         Next element
         :return:
         """
-        # Load if needed
-        while len(self._medias) == 0:
-            self._load_medias()
+        if len(self._medias) <= 0:
+            raise StopIteration()
         # end if
 
         # Current media
@@ -95,34 +92,5 @@ class MediaFinder(object):
     ############################################
     # Private
     ############################################
-
-    # Get medias
-    def _load_medias(self):
-        """
-        Get medias
-        :return: A tweet
-        """
-        # Get page
-        page = self._cursor.next()
-
-        # Get all tweets
-        for tweet in page:
-            # if not tweet.retweeted and 'RT @' not in tweet.text:
-            if not tweet.retweeted:
-                # Analyze text
-                tweet_blob = TextBlob(tweet.text)
-
-                # Pass level of pol & sub
-                if tweet_blob.sentiment.polarity >= self._polarity and \
-                    tweet_blob.sentiment.subjectivity <= self._subjectivity and \
-                    tweet_blob.detect_language() in self._languages:
-                    self._medias.append((tweet, tweet_blob.sentiment.polarity, tweet_blob.sentiment.subjectivity))
-                # end if
-        # end for
-
-        # Wait
-        logging.getLogger(pystr.LOGGER).info(u"Waiting 60 seconds...")
-        time.sleep(60)
-    # end _load_tweets
 
 # end TweetFinder
