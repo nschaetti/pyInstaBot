40eea00fb6690c46d0416fae3ed0ee5474e12bbc
Nils Schaetti <n.schaetti@gmail.com>
2018-04-10 20:12:27 +0200

    Filters

--- friends/FriendsFinder.py
+++ friends/FriendsFinder.py
@@ -31,7 +31,7 @@ class FriendsFinder(object):
 
         # Feed
         self._users = pyInstaBot.instagram.InstagramConnector().hashtag_feed(hashtag)
-        print(self._users['ranked_items'])
+        print(self._users['ranked_items'][0])
         exit()
         self._users = self._users['ranked_items']
 
