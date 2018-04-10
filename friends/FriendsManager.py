2f9903adcb9caa59cb79c8a26819271d4be36754
Nils Schaetti <n.schaetti@gmail.com>
2018-04-10 22:54:45 +0200

    Find unfollow

--- friends/FriendsManager.py
+++ friends/FriendsManager.py
@@ -314,7 +314,9 @@ class FriendsManager(object):
         """
         # For each follower
         for user in pyInstaBot.instagram.InstagramConnector().followers():
-            if not pyInstaBot.db.obj.User.exists(user.user_id):
+            print(user)
+            exit()
+            if not pyInstaBot.db.obj.User.exists(user[]):
                 logging.getLogger(pystr.LOGGER).info(u"New follower in the database : {}".format(user))
                 print(user)
                 exit()
