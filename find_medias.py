602537905e09fa82ca66075ed58e6ecd2eb936b0
nschaetti <n.schaetti@gmail.com>
2018-06-23 00:10:57 +0200

    Check None

--- find_medias.py
+++ find_medias.py
@@ -58,54 +58,56 @@ def find_medias(config, model_file, action_scheduler, action='comment', min_leng
     for hashtag in config.hashtags:
         # For each media
         for media in MediaFinder(search_keywords=hashtag, shuffle=True):
-            # Media's caption
-            media_code = media['code']
-            media_caption = media['caption']['text']
-            media_id = media['pk']
+            if media is not None:
+                # Media's caption
+                media_code = media['code']
+                media_caption = media['caption']['text']
+                media_id = media['pk']
 
-            # Predict class
-            censor_prediction, _ = censor(media_caption)
+                # Predict class
+                censor_prediction, _ = censor(media_caption)
 
-            # Debug
-            logging.getLogger(pystr.LOGGER).debug(
-                pystr.DEBUG_NEW_MEDIA_FOUND.format(hashtag, media_id)
-            )
+                # Debug
+                logging.getLogger(pystr.LOGGER).debug(
+                    pystr.DEBUG_NEW_MEDIA_FOUND.format(hashtag, media_id)
+                )
 
-            # Text
-            media_text_blob = TextBlob(media_caption)
+                # Text
+                media_text_blob = TextBlob(media_caption)
 
-            # Pass the censor
-            if len(media_caption) > min_length and censor_prediction == 'pos' and media_text_blob.detect_language() in \
-                    config.post['languages']:
-                # Comment
-                comment = u"Nice!"
+                # Pass the censor
+                if len(media_caption) > min_length and censor_prediction == 'pos' and media_text_blob.detect_language() in \
+                        config.post['languages']:
+                    # Comment
+                    comment = u"Nice!"
 
-                # Try to add
-                try:
-                    # Add action
-                    if action == 'comment':
-                        logging.getLogger(pystr.LOGGER).info(pystr.INFO_ADD_COMMENT_SCHEDULER.format(
-                            comment,
-                            media_id,
-                            media_code
-                        ))
-                        action_scheduler.add_comment(media_id, comment, media_code)
-                    else:
-                        logging.getLogger(pystr.LOGGER).info(pystr.INFO_ADD_LIKE_SCHEDULER.format(
-                            media_id,
-                            media_code
-                        ))
-                        action_scheduler.add_like(media_id, media_code)
-                    # end if
-                except ActionReservoirFullError:
-                    logging.getLogger(pystr.LOGGER).error(pystr.ERROR_RESERVOIR_FULL)
-                    exit()
-                    pass
-                except ActionAlreadyExists:
-                    logging.getLogger(pystr.LOGGER).error(pystr.ERROR_COMMENT_ALREADY_DB.format(
-                        media_id))
-                    pass
-                # end try
+                    # Try to add
+                    try:
+                        # Add action
+                        if action == 'comment':
+                            logging.getLogger(pystr.LOGGER).info(pystr.INFO_ADD_COMMENT_SCHEDULER.format(
+                                comment,
+                                media_id,
+                                media_code
+                            ))
+                            action_scheduler.add_comment(media_id, comment, media_code)
+                        else:
+                            logging.getLogger(pystr.LOGGER).info(pystr.INFO_ADD_LIKE_SCHEDULER.format(
+                                media_id,
+                                media_code
+                            ))
+                            action_scheduler.add_like(media_id, media_code)
+                        # end if
+                    except ActionReservoirFullError:
+                        logging.getLogger(pystr.LOGGER).error(pystr.ERROR_RESERVOIR_FULL)
+                        exit()
+                        pass
+                    except ActionAlreadyExists:
+                        logging.getLogger(pystr.LOGGER).error(pystr.ERROR_COMMENT_ALREADY_DB.format(
+                            media_id))
+                        pass
+                    # end try
+                # end if
             # end if
         # end for
     # end for
