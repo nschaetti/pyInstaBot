3a2d7710befe8083271327aff398c2e82b842516
nschaetti <n.schaetti@gmail.com>
2018-07-14 22:51:47 +0200

    Comment

--- find_medias.py
+++ find_medias.py
@@ -64,7 +64,8 @@ def find_medias(config, model_file, action_scheduler, action='comment', min_leng
                 media_code = media['code']
                 media_caption = media['caption']['text']
                 media_id = media['pk']
-
+                print(media)
+                exit()
                 # Predict class
                 censor_prediction, _ = censor(media_caption)
 
