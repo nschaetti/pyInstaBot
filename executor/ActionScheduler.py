c31494137e1834648715df78a56d05c2f03206b3
nschaetti <n.schaetti@gmail.com>
2018-07-15 14:48:30 +0200

    Location

--- executor/ActionScheduler.py
+++ executor/ActionScheduler.py
@@ -193,6 +193,7 @@ class ActionScheduler(object):
         :return:
         """
         if not self.exists_post_action(action_post_image=media_path, action_post_text=media_caption):
+            print(u"Media location : {}".format(media_location))
             action = pyInstaBot.db.obj.Action(
                 action_type='Post',
                 action_order=self._generate_random_order(),
